77fd63e326677799482d7c89645d71cfa3c07d02
Aastha <aastha.chaudhary237@gmail.com>
2022-02-20 06:45:12 +0530

    Add new buttons (primary, warning, danger etc ), remove switch toogle button and c(/\s+/) change in word count

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 import  {useState} from 'react';
-// import About from './components/About';
+import About from './components/About';
 
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 
 function App() {
@@ -26,40 +25,47 @@ function App() {
         setAlert(null)
     },1500);
   }
+  const removebodyclasses=()=>{
+    document.body.classList.remove('bg-Light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-primary')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+    document.body.classList.remove('bg-warning')
 
-  const togglemode = () => {
+
+  }
+  const togglemode = (cls) => {
+    removebodyclasses();
+    document.body.classList.add('bg-'+cls)
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
-      document.title="Textutils - Dark Mode";
-      // setText("Enable Light Mode")
       showAlert("Dark mode has been enabled","success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      // setText("Enable Dark Mode")
-      document.title="Textutils - Light Mode"; 
       showAlert("Light mode has been enabled","success");
     }
   }
   return (
     <>
-      {/*<Router>*/}
+      <Router>
         <Navbar title="Textutils2" About="about" Mode={mode} togglemode ={togglemode} / >
         <Alert alert={alert}/>
         <div className="container">
-       {/*} <Switch>
+       <Switch>
             <Route exact path="/about">
-              <About />
+              <About mode={mode}/>
             </Route>
-  <Route exact path="/">*/}
-              <TextForm  showAlert={showAlert} heading ="Enter the text to analyze" Mode={mode}/>
-{/*</Route>}
-</Switch>*/}
+  <Route exact path="/">
+              <TextForm  showAlert={showAlert} heading =" Textutils - Word Counter, character counter" Mode={mode}/>
+</Route>
+</Switch>
 
         </div> 
-      {/*</Router>*/}
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,7 +1,8 @@
-import {useState} from 'react'
+import react, {useState} from 'react'
 
 
 export default function TextForm(props){
+    
     const handleUpClick=()=>{
         console.log("uppercase" + text);
         let newText = text.toUpperCase();
@@ -19,24 +20,31 @@ export default function TextForm(props){
         props.showAlert("converted to lowerCase","Success")
 
     }
-    const[text,setText] = useState('Enter text here');
+    const handleCopy = () =>{
+        navigator.clipboard.writeText(text);
+        props.showAlert("copy to clipboard","Success")
+
+    }
+
+    const[text,setText] = useState('');
     return(
         <>
         <div className="container" style ={{color: props.Mode === 'dark'?'white':'#042743'}}> 
             <div className="mb-3">
-                <h1 >{props.heading}</h1>
-                <textarea className ="form-control" id="mybox" rows="8" value = {text} onChange={handleOnChange} style={{backgroundColor: props.Mode === 'dark' ? 'gray':'white', color: props.Mode === 'dark'?'white':'black'}}></textarea>
+                <h1 className='mb-4'>{props.heading}</h1>
+                <textarea className ="form-control" id="mybox" rows="8"  onChange={handleOnChange} style={{backgroundColor: props.Mode === 'dark' ? 'rgb(51 79 102)':'white', color: props.Mode === 'dark'?'white':'black'}}></textarea>
             </div> 
-            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleDownClick}>Convert to Lowercase</button>
+            <button disabled = {text.length==0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled = {text.length==0} className="btn btn-primary mx-2 my-1" onClick={handleDownClick}>Convert to Lowercase</button>
+            <button disabled = {text.length==0} className="btn btn-primary mx-2 my-1" onClick={handleCopy}>Copy To Clipboard</button>
 
         </div>
         <div className="container my-2" style={{color: props.Mode === 'dark'?'white':'#042743'}}>
             <h1>Your Text summary</h1>
-            <p>{text.split(" ").length} words , {text.length} characters</p>
-            <p>{0.008*text.split("").length} Minutes read </p>
-            <h2>{text.length>0?text:"Enter something in the textbox above to preview it her"}</h2> 
-            <p>{text}</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words , {text.length} characters</p>
+            <p>{0.008*text.split("").filter((element)=>{return element.length!==0}).length} Minutes read </p>
+            <h2>Preview</h2> 
+            <p>{text.length>0?text:"Nothing to preview"}</p>
         </div>
         </>
     )
